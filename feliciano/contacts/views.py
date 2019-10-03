41e02f535d1e4ef777d7fc6f798be2ee884dc32a
pentest225 <47216215+pentest225@users.noreply.github.com>
2019-10-03 09:10:34 +0000

    OK

--- feliciano/contacts/views.py
+++ feliciano/contacts/views.py
@@ -1,25 +1,34 @@
 from django.shortcuts import render
 from .models import *
+from mainConfig.models import contact
 # Create your views here.
-def contact(request):
+def contactfonc(request):
+    
+    contacts=contact.objects.filter(status=True)[:1].get()
+
     myName=request.POST.get('name',False)
     myEmail=request.POST.get('email',False)
-    mySubject=request.POST.get('suject',False)
+    mySubject=request.POST.get('subject',False)
     myMessage=request.POST.get('message',False)
     isSave=False
+    print(myEmail,myName,mySubject,myMessage)
+
     if request.method == 'POST':
         if myName is not False and myEmail is not False and mySubject is not False:
+           
+            isSave=True
             try:
                 print(myEmail,myName,mySubject,myMessage)
-                newMessage=Message(name=myName,email=myEmail,suject=mySubject,message=myMessage)
+                newMessage=Message(name=myName,email=myEmail,sujet=mySubject,message=myMessage)
                 newMessage.save()
                 isSave=True
                 print('message save ')
-            except:
+            except :
                 isSave=False
-                print('erreur save message ')
+                print('erreur de sauvegarde ')
                 
     data={
-        'error':isSave
+        'error':isSave,
+        'contact':contacts,
     }
     return render(request,'pages/contact.html',data)
